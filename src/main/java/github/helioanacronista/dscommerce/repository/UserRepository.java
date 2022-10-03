67dbe87da02e0abb2060facaeea33a8a18e920ba
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-03 18:04:32 -0300

    BCrypt

--- src/main/java/github/helioanacronista/dscommerce/repository/UserRepository.java
+++ src/main/java/github/helioanacronista/dscommerce/repository/UserRepository.java
@@ -3,7 +3,7 @@ package github.helioanacronista.dscommerce.repository;
 import github.helioanacronista.dscommerce.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface UserRepository extends JpaRepository<UserRepository, Long> {
+public interface UserRepository extends JpaRepository<User, Long> {
 
    User findByEmail(String email);
 }
