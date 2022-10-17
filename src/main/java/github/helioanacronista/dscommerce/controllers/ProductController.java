74fd7ed73d7f1b31e09dd26494043a143f2a1634
Hélio Fernandes <104803451+HelioAnacronista@users.noreply.github.com>
2022-10-17 19:00:20 -0300

    add : ProductMinDTO.

--- src/main/java/github/helioanacronista/dscommerce/controllers/ProductController.java
+++ src/main/java/github/helioanacronista/dscommerce/controllers/ProductController.java
@@ -29,8 +29,8 @@ public class ProductController {
     }
 
     @GetMapping
-    public ResponseEntity<Page<ProductDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
-        Page<ProductDTO> dto = productService.findAll(name, pageable);
+    public ResponseEntity<Page<ProductMinDTO>> findAll(@RequestParam(name = "name", defaultValue = "") String name, Pageable pageable) {
+        Page<ProductMinDTO> dto = productService.findAll(name, pageable);
         return ResponseEntity.ok(dto);
     }
 
